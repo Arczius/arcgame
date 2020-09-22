ea412604224d9f0136da6293069751d21ff372ab
Damian Vaartmans <99061149@mydavinci.nl>
2020-09-22 19:48:55 +0200

    update js

--- js/game.js
+++ js/game.js
@@ -1,2 +1,11 @@
 alert("Welcome to the Arc Game");
 alert("its a text Game");
+alert("it will show options with the numbers like 1, 2, 3");
+var test = prompt("select the correct number if you understand. " + "\n" + "1: yes" + "\n" + "2: no");
+
+if (test == "1"){
+	alert("good job")
+}
+else if(test == "2"){
+	alert("sorry it doesnt work like that")
+}
