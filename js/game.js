5967b61eed205a691f4ed433cf19d0bf52787f75
Damian Vaartmans <99061149@mydavinci.nl>
2020-09-25 08:52:59 +0200

    updated story added css

--- js/game.js
+++ js/game.js
@@ -4,8 +4,11 @@ alert("it will show options with the numbers like 1, 2, 3");
 var test = prompt("select the correct number if you understand. " + "\n" + "1: yes" + "\n" + "2: no");
 
 if (test == "1"){
-	alert("good job")
+	alert("good job");
+	var fname = prompt("what is your character's first name");
+	var lname = prompt("what is your character's last name");
+	alert("hello " + fname + " " + lname + "," + "\n" + "Welcome to the Arc Game");
 }
 else if(test == "2"){
-	alert("sorry it doesnt work like that")
+	alert("sorry it doesnt work like that");
 }
