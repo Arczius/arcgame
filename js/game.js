bb3996adc655b3b7722883c6e5ab00a57c2429c2
Arc <arcvaartmans@gmail.com>
2020-09-26 12:17:01 +0200

    added some story

--- js/game.js
+++ js/game.js
@@ -1,5 +1,9 @@
-alert("Welcome to the Arc Game");
+//Damian Vaartmans
+//99061149
+//software development
+
 alert("WARNING THIS GAME ISNT FOR PEOPLE EASILY DISTURBED");
+alert("Welcome to the Arc Game");
 alert("its a text Game");
 alert("it will show options with the numbers like 1, 2, 3");
 var test = prompt("select the correct number if you understand. " + "\n" + "1: yes" + "\n" + "2: no");
@@ -9,6 +13,7 @@ if (test == "1"){
 }
 else if(test == "2"){
 	alert("sorry it doesnt work like that");
+	alert("but you will be able to just continue the game");
 }
 
 var fname = prompt("what is your character's first name");
@@ -22,8 +27,14 @@ var sleep1 = prompt("its a cold morning, you are in your bed, its time to get up
 	if (sleep1 == "1") {
 		alert("you decide to wake up");
 		alert("when you go downstairs you see your mother, she sees you too and says good morning kiddo");
-		alert("today we are going to see the Mosty Tower");
+		alert("today we are going to see the Mosty Tower");		
 		var resp1 = prompt("so what do you think of that kiddo?" + "\n" + "1: thats so cool" + "\n" + "2: no thanks id rather stay home")
+		if(resp1 == "1"){
+			alert("to the Mosty Tower we go");
+			alert("once we get there you see your aunt");
+			alert("she says hello " + fname + ", how are you?");
+			var howru = prompt("how do you respond?" + "\n" + "1: im good, thanks" + "\n" + "2: i had a really weird dream about you two killing me");
+		}
 	}
 	else if (sleep1 == "2") {
 		alert("you go back go sleep");
@@ -31,6 +42,9 @@ var sleep1 = prompt("its a cold morning, you are in your bed, its time to get up
 		var yell1 = prompt("its your mom, she is yelling at you for oversleeping, what do you do?" + "\n" + "1: apologize to her" + "\n" + "2 yell back at her");
 		if(yell1 == "1"){
 		 	alert("you apologize to her" + "\n" + "she says that it is ok," + "\n" + "she has a special plan for today");
+		 	alert("she tells you that you two are going to the pool");
+		 	var pool1 = prompt("do you want to go with your mom to the pool?" + "\n" + "1: yes ofcourse" + "\n" + "2: no i dont want to go to the pool");
+
 		}
 		else if(yell1 == "2"){
 			alert("you yell back at your mom");
